7ce55c19ea3f518c699765816d44cf175696c560
Torikul <toriqulislam142@gmail.com>
2022-04-28 23:42:20 -0500

    created simple pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Footer from './components/pages/Footer/Footer';
+import Home from './components/pages/Home/Home';
+import Navbar from './components/pages/Navbar/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello</h1>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+      <Footer />
+      
+
     </div>
   );
 }
