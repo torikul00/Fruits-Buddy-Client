511b54e3d1349b2690f2db9c1d58d0944f5457b8
Torikul <toriqulislam142@gmail.com>
2022-04-30 00:30:37 -0500

    error handle for login form

--- src/components/pages/Login/Login.js
+++ src/components/pages/Login/Login.js
@@ -1,22 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Login.css'
 import { FcGoogle } from 'react-icons/fc';
 
 const Login = () => {
+    const [email,setEmail] = useState({value:'',error:''})
+    const [password,setPassword] = useState({value:'',error:''})
+
+    const handleEmail = email => {
+
+        if (/^\S+@\S+\.\S+$/.test(email)) {
+
+            setEmail({ value: email, error: '' })
+        }
+       else if (email === '') {
+            setEmail({ value:'',error:'Email required'})
+        }
+        else {
+            setEmail({ value: '', error: 'Please check your email' })
+        }
+    }
+    const handlePassword = password => {
+        if (password === '') {
+            setPassword({ value:'',error:'Password required'})
+        }
+        else if (password.length < 6) {
+            setPassword({ value: '', error: 'Password is too short ' })
+        }
+        else { setPassword({ value: password, error: '' }) }
+    }
 
     const handleSubmit = e => {
         e.preventDefault()
-        const email = e.target.email.value
-        const password = e.target.password.value
+        
 
     }
     return (
         <div className='form-container'>
             <form onSubmit={handleSubmit}>
                 <h1> Login Here</h1>
-                <input name='email' type="text" placeholder='Email' required />
-                <input name='password' type="text" placeholder='Password' required />
+                <input onBlur={(e)=>handleEmail(e.target.value)} type="text" placeholder='Email' required />
+                <input onBlur={(e)=>handlePassword(e.target.value)} type="text" placeholder='Password' required />
                 <button className='login-button' type='submit'>Login</button>
                 <p>New user ? <Link className='form-link' to='/signUp'>SignUp </Link> </p>
                 <div className="horizontal-line">
