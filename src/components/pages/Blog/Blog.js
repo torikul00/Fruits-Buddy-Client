09aa04c3d9bd313caa7a8386f04089749739fb18
Torikul <toriqulislam142@gmail.com>
2022-04-30 02:35:09 -0500

    completed writing blogs

--- src/components/pages/Blog/Blog.js
+++ src/components/pages/Blog/Blog.js
@@ -6,21 +6,22 @@ const Blog = () => {
         <div>
             <div className="blog-container">    
                 <div className="blog">
-                    <h2>What is different between JavaScript ans NodeJs ?</h2>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos suscipit adipisci corrupti quam vel, ex dolor totam rerum. Tempora voluptate impedit aut at non cum totam, neque maiores. Sapiente reprehenderit libero est necessitatibus recusandae velit aperiam doloremque asperiores quae sunt.</p> <hr />
+                    <h2>What is different between JavaScript and NodeJs ?</h2>
+                    <p>JavaScipt is programming language. It is runs web browsers.NodeJs is a javascirpt runtime enviroment.NodeJS use for communicate the backend server.NodeJs helps run javascirpt code without web browser.NodeJs use V8 engine for run javascirpt code outside the browser . Javascipt use for client side but NodeJs use for server side . </p> <hr />
                    
                 </div>
                 <div className="blog">
                     <h2>When should you use NodeJs and When should you use mongoDB ?</h2>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos suscipit adipisci corrupti quam vel, ex dolor totam rerum. Tempora voluptate impedit aut at non cum totam, neque maiores. Sapiente reprehenderit libero est necessitatibus recusandae velit aperiam doloremque asperiores quae sunt.</p> <hr />
+                    <p>When i nedd to call api from client side ,then i have to use NodeJs for creating API . When we need to store data ,then we use mongodb for storing data
+                    without NodeJs we can not connect with mongodb database system.</p> <hr />
                 </div>
                 <div className="blog">
                     <h2>Different between SQL and NoSQL ?</h2>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos suscipit adipisci corrupti quam vel, ex dolor totam rerum. Tempora voluptate impedit aut at non cum totam, neque maiores. Sapiente reprehenderit libero est necessitatibus recusandae velit aperiam doloremque asperiores quae sunt.</p> <hr />
+                    <p>SQL is Vertically Scalable but NoSQL is horizontal Scalable.SQL database system are good for complex queries But NoSQL database system are not good for complex queries. <br /> SQL are fixed and static schema but NoSQL have dyanamically schema.SQL is tablebase database system , but NoSQL is document ,key database system.</p> <hr />
                 </div>
                 <div className="blog">
                     <h2>What is the purpose of JWT and how it works ?</h2>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos suscipit adipisci corrupti quam vel, ex dolor totam rerum. Tempora voluptate impedit aut at non cum totam, neque maiores. Sapiente reprehenderit libero est necessitatibus recusandae velit aperiam doloremque asperiores quae sunt.</p> <hr />
+                    <p>JWT- stand for Json Web Token . </p> <hr />
                 </div>
             </div>
         </div>
